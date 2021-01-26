47f6edd080126d349694fb7eaefa4fc1bf929ad0
truth809 <truth809@naver.com>
2021-01-26 09:46:05 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [number, setNumber] = useState(1);
+
+  const add = () => setNumber(number => number + 1);
+  const subtract = () => setNumber(number => number - 1);
+  const multplayby2 = () => setNumber(number => number * 2);
+  const multplayby2AndAddby1 = () => {
+    multplayby2();
+    add();
+  };
+  // const currentState = {
+  //   number : 1
+  // }
+  // const newState = Object.assign(currentState, { number : number * 2}, { number : number + 1})
+  // setNumber(newState)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Number : {number}</h1>
+      <div className="btn-wrapper">
+        <button onClick={add}>+ 1</button>
+        <button onClick={subtract}>- 1</button>
+        <button onClick={multplayby2}>* 2</button>
+        <button onClick={multplayby2AndAddby1}>*2 + 1</button>
+      </div>
     </div>
   );
 }
